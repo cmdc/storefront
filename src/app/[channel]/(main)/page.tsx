eae7b834349ea1110c49122a1f4269c193764f4d
Your name <samuel.pascale@01s.it>
2026-03-06 17:56:59 +0100

    first commit

--- src/app/[channel]/(main)/page.tsx
+++ src/app/[channel]/(main)/page.tsx
@@ -5,7 +5,7 @@ import { executePublicGraphQL } from "@/lib/graphql";
 import { ProductList } from "@/ui/components/product-list";
 
 export const metadata = {
-	title: "ACME Storefront, powered by Saleor & Next.js",
+	title: "Sophie Coffe",
 	description:
 		"Storefront Next.js Example for building performant e-commerce experiences with Saleor - the composable, headless commerce platform for global brands.",
 };
